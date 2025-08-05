971087fe6b61f648ebdd1bde6d631aead782ad9a
Kareem Yaqoup <yaqoup.kareem@gmail.com>
2025-08-05 22:53:14 +0300

    Add "Add Product" Functionality to Inventory.cs
    Added the ability to add a product to the inventory

---
+++ InventoryLibrary/Models/Inventory.cs
@@ -0,0 +1,20 @@
+﻿namespace InventoryLibrary.Models;
+
+public class Inventory
+{
+    private static List<Product> _products = new();
+
+    public static bool IsExist(String name)
+    {
+        Product? exist = _products.Find(p => p.Name == name);
+        return exist != null;
+    }
+    
+    public static bool AddProduct(Product product)
+    {
+        if (IsExist(product.Name)) return false;
+        
+        _products.Add(product);
+        return true;
+    }
+}
