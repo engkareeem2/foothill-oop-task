a20b8611f303aaf8405afcccc4b68f1cf275d0eb
Kareem Yaqoup <yaqoup.kareem@gmail.com>
2025-08-05 16:06:23 +0300

    Create Product Class

---
+++ FoothillOOPTask/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace FoothillOOPTask.Models;
+
+public class Product
+{
+    public String Name { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+
+    public Product()
+    {
+        this.Name = String.Empty;
+        this.Price = Int16.MaxValue;
+        this.Quantity = 0;
+    }
+
+    public Product(String name, decimal price, int quantity)
+    {
+        this.Name = name;
+        this.Price = price;
+        this.Quantity = quantity;
+    }
+}
