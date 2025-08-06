14109ba87a533a4155acbb0a6097d8a1f072c238
Kareem Yaqoup <yaqoup.kareem@gmail.com>
2025-08-06 12:41:39 +0300

    Refactor Main Menu Printing
    Refactored Printing MainMenu to improve readability

--- Interface.ConsoleApp/Program.cs
+++ Interface.ConsoleApp/Program.cs
@@ -2,20 +2,9 @@
 using FoothillOOPTask.Utility;
 using InventoryLibrary.Models;
 
-StringBuilder mainMenu = new();
-
-mainMenu.Append("------------------- [ Main Menu ] -------------------\n");
-mainMenu.Append("\t=> Pick an option <= \n");
-mainMenu.Append("=> 1. Add Product\n");
-mainMenu.Append("=> 2. View All Products\n");
-mainMenu.Append("=> 3. Edit a Product\n");
-mainMenu.Append("=> 4. Delete a Product\n");
-mainMenu.Append("=> 5. Search for a Product\n");
-
-
 while (true)
 {
-    Console.Write(mainMenu);
+    UserInterface.PrintMain();
     int option = ConsoleUtils.ReadInteger("Select an option: ", null);
 
     switch (option)
--- Interface.ConsoleApp/Utility/UserInterface.cs
+++ Interface.ConsoleApp/Utility/UserInterface.cs
@@ -4,6 +4,17 @@ namespace FoothillOOPTask.Utility;
 
 public class UserInterface
 {
+    public static void PrintMain()
+    {
+        Console.WriteLine("------------------- [ Main Menu ] -------------------");
+        Console.WriteLine("\t=> Pick an option <= ");
+        Console.WriteLine("=> 1. Add Product");
+        Console.WriteLine("=> 2. View All Products");
+        Console.WriteLine("=> 3. Edit a Product");
+        Console.WriteLine("=> 4. Delete a Product");
+        Console.WriteLine("=> 5. Search for a Product");
+    }
+    
     public static void HandleAddProduct()
     {
         String name = ConsoleUtils.ReadConsole("Enter Product's Name: ", null);
